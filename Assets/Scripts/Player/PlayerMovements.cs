6d497352bd1bb2c3dcaabf241789087f98fca893
LeLapinouAlpha <t-henon@hotmail.fr>
2026-03-09 14:54:04 +0100

    feat: add basic Idle/Walking animation

--- Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Animations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,35 +7,39 @@ public class PlayerMovements : MonoBehaviour
     [Header("Components references")]
     public CharacterController characterController;
     public InputAction moveAction;
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
 
     [Header("Controls settings")]
     public float movementSpeed = 0.02f;
-
-    [Header("States")]
-    public bool isMoving;
 
     void Start()
     {
         // Get components attached to the player
         this.characterController = this.GetComponent<CharacterController>();
+        this.animator = this.GetComponent<Animator>();
+        this.spriteRenderer = this.GetComponent<SpriteRenderer>();
 
         // Find input actions
         this.moveAction = InputSystem.actions.FindAction("Move");
     }
 
-    // Update is called once per frame
     void Update()
     {
         // Read move input and move the character and update the isMoving state
         var movement = this.moveAction.ReadValue<Vector2>();
         if (movement != Vector2.zero)
         {
-            characterController.Move(this.movementSpeed * movement.x * Vector2.right);
-            this.isMoving = true;
+            this.characterController.Move(this.movementSpeed * movement.x * Vector2.right);
+
+            this.animator.SetBool("IsWalking", true);
+
+            // Flip the character sprite based on the movement direction
+            this.spriteRenderer.flipX = movement.x < 0;
         }
         else
         {
-            this.isMoving = false;
+            this.animator.SetBool("IsWalking", false);
         }
     }
 }
