e9b395fd49d21ecaca85ae07d4f3acbf8749ac56
LeLapinouAlpha <t-henon@hotmail.fr>
2026-03-10 14:16:56 +0100

    feat: add player sprint movement

--- Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -4,14 +4,18 @@ using UnityEngine.InputSystem;
 public class PlayerMovements : MonoBehaviour
 {
     [Header("Components references")]
-    public InputAction moveAction;
-    public InputAction jumpAction;
     public PlayerAnimations playerAnimations;
     public Rigidbody2D playerRigidbody;
     public CapsuleCollider2D playerCollider;
 
+    [Header("Movements actions")]
+    public InputAction walkAction;
+    public InputAction jumpAction;
+    public InputAction sprintAction;
+
     [Header("Controls settings")]
     public float walkSpeed = 3f;
+    public float sprintSpeedMultiplier = 2f;
     public float jumpForce = 7f;
     public float defaultGravityScale = 1f;
     public float fallGravityScale = 3f;
@@ -27,6 +31,7 @@ public class PlayerMovements : MonoBehaviour
     public bool isWalking;
     public bool isJumping;
     public bool isFalling;
+    public bool isSprinting;
 
     private void MovePlayer()
     {
@@ -50,8 +55,9 @@ public class PlayerMovements : MonoBehaviour
         this.playerCollider = this.GetComponent<CapsuleCollider2D>();
 
         // Find input actions
-        this.moveAction = InputSystem.actions.FindAction("Move");
+        this.walkAction = InputSystem.actions.FindAction("Move");
         this.jumpAction = InputSystem.actions.FindAction("Jump");
+        this.sprintAction = InputSystem.actions.FindAction("Sprint");
 
         // Set default gravity scale
         this.playerRigidbody.gravityScale = this.defaultGravityScale;
@@ -67,11 +73,11 @@ public class PlayerMovements : MonoBehaviour
     void Update()
     {
         // Get movement input and update player states with animations
-        var moveInput = this.moveAction.ReadValue<Vector2>();
-        this.movement.x = this.walkSpeed * moveInput.x;
-        this.isWalking = Mathf.Abs(moveInput.x) > 1e-6f;
+        var walkInput = this.walkAction.ReadValue<Vector2>();
+        this.movement.x = this.walkSpeed * walkInput.x;
+        this.isWalking = Mathf.Abs(walkInput.x) > 1e-6f;
         this.playerAnimations.PlayWalkAnimation(this.isWalking);
-        this.playerAnimations.FlipSprite(moveInput.x < 0);
+        this.playerAnimations.FlipSprite(walkInput.x < 0);
 
         // Check if player is jumping and update states with animations
         this.isJumping = this.jumpAction.IsPressed() && this.isGrounded;
@@ -87,6 +93,13 @@ public class PlayerMovements : MonoBehaviour
         // Check if player is falling and update states with animations
         this.isFalling = this.playerRigidbody.linearVelocity.y < 0 && !this.isGrounded;
         this.playerAnimations.PlayFallingAnimation(this.isFalling);
+
+        // Check if player is sprinting and update states with animations
+        this.isSprinting = this.sprintAction.IsPressed() && this.isWalking;
+        if (this.isSprinting)
+        {
+            this.movement.x *= this.sprintSpeedMultiplier;
+        }
     }
 
     private void OnDrawGizmos()
