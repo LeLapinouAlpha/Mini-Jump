966483392f86882bffb5c65eed25a01337c39764
LeLapinouAlpha <t-henon@hotmail.fr>
2026-03-11 15:27:31 +0100

    feat(PlayerMovements): implement wall check

--- Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -25,6 +25,12 @@ public class PlayerMovements : MonoBehaviour
     public Transform groundCheck;
     public float groundCheckRadius = 0.1f;
 
+    [Header("Wall check")]
+    public LayerMask wallLayers;
+    public Transform wallCheckLeft;
+    public Transform wallCheckRight;
+    public float wallCheckRadius = 0.1f;
+
     [Header("States")]
     public Vector2 movement;
     public bool isGrounded;
@@ -32,6 +38,7 @@ public class PlayerMovements : MonoBehaviour
     public bool isJumping;
     public bool isFalling;
     public bool isSprinting;
+    public bool isTouchingWall;
 
     private void MovePlayer()
     {
@@ -54,6 +61,11 @@ public class PlayerMovements : MonoBehaviour
         this.playerRigidbody = this.GetComponent<Rigidbody2D>();
         this.playerCollider = this.GetComponent<CapsuleCollider2D>();
 
+        // Get ground and wall check transforms from child objects
+        this.groundCheck = this.transform.Find("GroundCheck");
+        this.wallCheckLeft = this.transform.Find("WallCheckLeft");
+        this.wallCheckRight = this.transform.Find("WallCheckRight");
+
         // Find input actions
         this.walkAction = InputSystem.actions.FindAction("Move");
         this.jumpAction = InputSystem.actions.FindAction("Jump");
@@ -65,8 +77,17 @@ public class PlayerMovements : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Check if player is grounded and update states
         this.isGrounded = Physics2D.OverlapCircle(this.groundCheck.position, this.groundCheckRadius, this.groundLayers);
 
+        // Check if player is touching wall with two raycasts
+        var isTouchingWallLeft = Physics2D.Raycast(this.wallCheckLeft.position, Vector2.left, this.wallCheckRadius, this.wallLayers);
+        //Debug.DrawRay(this.wallCheckLeft.position, Vector2.left * this.wallCheckRadius, Color.blue, 0.5f);
+        var isTouchingWallRight = Physics2D.Raycast(this.wallCheckRight.position, Vector2.right, this.wallCheckRadius, this.wallLayers);
+        //Debug.DrawRay(this.wallCheckRight.position, Vector2.right * this.wallCheckRadius, Color.blue, 0.5f);
+        this.isTouchingWall = isTouchingWallRight || isTouchingWallLeft;
+
+        // Move player based on input and states
         this.MovePlayer();
     }
 
@@ -97,13 +118,19 @@ public class PlayerMovements : MonoBehaviour
         // Check if player is sprinting and update states with animations
         this.isSprinting = this.sprintAction.IsPressed() && this.isWalking;
         this.movement.x *= this.isSprinting ? this.sprintSpeedMultiplier : 1f;
- 
+
         this.playerAnimations.PlaySprintAnimation(this.isSprinting);
     }
 
     private void OnDrawGizmos()
     {
+        // Draw ground check gizmos as a red wire sphere
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(this.groundCheck.position, this.groundCheckRadius);
+
+        // Draw wall check gizmos as lines
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(this.wallCheckLeft.position, this.wallCheckLeft.position + Vector3.left * this.wallCheckRadius);
+        Gizmos.DrawLine(this.wallCheckRight.position, this.wallCheckRight.position + Vector3.right * this.wallCheckRadius);
     }
 }
