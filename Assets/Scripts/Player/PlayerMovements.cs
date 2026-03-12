cb725bd73c3345e1b87333dbf4b29e339ec49b3e
LeLapinouAlpha <t-henon@hotmail.fr>
2026-03-12 12:43:21 +0100

    refactor: create lots of methods for player movements actions & rendering

--- Assets/Scripts/Player/PlayerMovements.cs
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -4,19 +4,21 @@ using UnityEngine.InputSystem;
 public class PlayerMovements : MonoBehaviour
 {
     [Header("Components references")]
-    public PlayerAnimations playerAnimations;
-    public Rigidbody2D playerRigidbody;
-    public CapsuleCollider2D playerCollider;
+    private PlayerAnimations playerAnimations;
+    private Rigidbody2D playerRigidbody;
+    private CapsuleCollider2D playerCollider;
 
     [Header("Movements actions")]
-    public InputAction walkAction;
-    public InputAction jumpAction;
-    public InputAction sprintAction;
+    private InputAction walkAction;
+    private InputAction jumpAction;
+    private InputAction sprintAction;
 
     [Header("Controls settings")]
     public float walkSpeed = 3f;
     public float sprintSpeedMultiplier = 2f;
     public float jumpForce = 7f;
+    public float horizontalWallJumpForce = 5f;
+    public float verticalWallJumpForce = 7f;
     public float defaultGravityScale = 1f;
     public float fallGravityScale = 3f;
 
@@ -35,26 +37,16 @@ public class PlayerMovements : MonoBehaviour
     public Vector2 movement;
     public bool isGrounded;
     public bool isWalking;
-    public bool isJumping;
+    public bool isGroundJumping;
     public bool isFalling;
     public bool isSprinting;
     public bool isTouchingWall;
+    public Vector2 wallNormal;
+    public Vector2 wallJumpDirection;
+    public Vector2 wallJumpForce;
+    public bool jumpRequested;
 
-    private void MovePlayer()
-    {
-        // Move player
-        this.playerRigidbody.linearVelocity = new Vector2(this.movement.x, this.playerRigidbody.linearVelocityY);
-
-        // Add jump force if player is jumping
-        if (this.isJumping)
-        {
-            this.playerRigidbody.AddForce(new Vector2(0, this.jumpForce), ForceMode2D.Impulse);
-            this.playerRigidbody.gravityScale = this.fallGravityScale;
-            this.isJumping = false;
-        }
-    }
-
-    void Start()
+    private void FindComponents()
     {
         // Get components attached to the player
         this.playerAnimations = this.GetComponent<PlayerAnimations>();
@@ -65,61 +57,151 @@ public class PlayerMovements : MonoBehaviour
         this.groundCheck = this.transform.Find("GroundCheck");
         this.wallCheckLeft = this.transform.Find("WallCheckLeft");
         this.wallCheckRight = this.transform.Find("WallCheckRight");
+    }
 
-        // Find input actions
-        this.walkAction = InputSystem.actions.FindAction("Move");
-        this.jumpAction = InputSystem.actions.FindAction("Jump");
-        this.sprintAction = InputSystem.actions.FindAction("Sprint");
-
+    private void InitializeComponents()
+    {
         // Set default gravity scale
         this.playerRigidbody.gravityScale = this.defaultGravityScale;
     }
 
-    private void FixedUpdate()
+    private void FindInputActions()
     {
-        // Check if player is grounded and update states
+        this.walkAction = InputSystem.actions.FindAction("Move");
+        this.jumpAction = InputSystem.actions.FindAction("Jump");
+        this.sprintAction = InputSystem.actions.FindAction("Sprint");
+    }
+
+    private void GroundCheck()
+    {
         this.isGrounded = Physics2D.OverlapCircle(this.groundCheck.position, this.groundCheckRadius, this.groundLayers);
 
-        // Check if player is touching wall with two raycasts
-        var isTouchingWallLeft = Physics2D.Raycast(this.wallCheckLeft.position, Vector2.left, this.wallCheckRadius, this.wallLayers);
-        //Debug.DrawRay(this.wallCheckLeft.position, Vector2.left * this.wallCheckRadius, Color.blue, 0.5f);
-        var isTouchingWallRight = Physics2D.Raycast(this.wallCheckRight.position, Vector2.right, this.wallCheckRadius, this.wallLayers);
-        //Debug.DrawRay(this.wallCheckRight.position, Vector2.right * this.wallCheckRadius, Color.blue, 0.5f);
-        this.isTouchingWall = isTouchingWallRight || isTouchingWallLeft;
+        // Check if player is grounded, then restore gravity scale and reset vertical velocity
+        if (this.isGrounded)
+        {
+            this.playerRigidbody.gravityScale = this.defaultGravityScale;
+            this.playerRigidbody.linearVelocityY = 0f;
+        }
+    }
 
-        // Move player based on input and states
+    private void WallCheck()
+    {
+        var leftWallHit = Physics2D.Raycast(this.wallCheckLeft.position, Vector2.left, this.wallCheckRadius, this.wallLayers);
+        var rightWallHit = Physics2D.Raycast(this.wallCheckRight.position, Vector2.right, this.wallCheckRadius, this.wallLayers);
+        this.isTouchingWall = rightWallHit || leftWallHit;
+        this.wallNormal = leftWallHit ? leftWallHit.normal : (rightWallHit ? rightWallHit.normal : Vector2.zero);
+    }
+
+    private void GroundJump()
+    {
+        this.playerRigidbody.AddForce(new Vector2(0, this.jumpForce), ForceMode2D.Impulse);
+        this.playerRigidbody.gravityScale = this.fallGravityScale;
+        this.isGroundJumping = false;
+    }
+
+    private void WallJump()
+    {
+        // TODO
+    }
+
+    private void MovePlayer()
+    {
+        // Add jump force if player is jumping
+        if (this.isGroundJumping)
+        {
+            this.GroundJump();
+        }
+
+        // Move player
+        this.playerRigidbody.linearVelocityX = this.movement.x;
+    }
+
+    private void UpdateWalk()
+    {
+        // Update walk input and states with animations
+        var walkInput = this.walkAction.ReadValue<Vector2>();
+        this.movement.x = this.walkSpeed * walkInput.x;
+
+        this.isWalking = Mathf.Abs(walkInput.x) > 1e-6f;
+
+        this.playerAnimations.PlayWalkAnimation(this.isWalking);
+        this.playerAnimations.FlipSprite(walkInput.x < 0);
+    }
+    private void UpdateSprint()
+    {
+        // Check if player is sprinting and update states with animations
+        this.isSprinting = this.sprintAction.IsPressed() && this.isWalking;
+
+        this.movement.x *= this.isSprinting ? this.sprintSpeedMultiplier : 1f;
+
+        this.playerAnimations.PlaySprintAnimation(this.isSprinting);
+    }
+
+    private void UpdateGroundJump()
+    {
+        // Check if player is jumping and update states with animations
+        this.isGroundJumping = this.jumpAction.IsPressed() && this.isGrounded;
+
+        this.playerAnimations.PlayJumpAnimation(this.isGroundJumping);
+    }
+
+    private void UpdateWallJump()
+    {
+        // TODO
+        if (this.isTouchingWall && !this.isGrounded)
+        {
+            this.wallJumpDirection = new Vector2(this.wallNormal.x, 1).normalized;
+            this.wallJumpForce = new Vector2(this.horizontalWallJumpForce * this.wallJumpDirection.x, this.verticalWallJumpForce * this.wallJumpDirection.y);
+
+            // Debug ray cast
+            Debug.DrawRay(this.transform.position, this.wallJumpForce, Color.black, 0.1f);
+        }
+    }
+
+    private void UpdateFall()
+    {
+        // Check if player is falling and update states with animations
+        this.isFalling = this.playerRigidbody.linearVelocity.y < 0 && !this.isGrounded;
+
+        this.playerAnimations.PlayFallingAnimation(this.isFalling);
+    }
+
+    void Start()
+    {
+        // Find components in the player object and its children
+        this.FindComponents();
+
+        // Initialize components
+        this.InitializeComponents();
+
+        // Find input actions
+        this.FindInputActions();
+    }
+
+    private void FixedUpdate()
+    {
+        // Check if player is grounded
+        this.GroundCheck();
+
+        // Check if player is touching wall
+        this.WallCheck();
+
+        // Move player based on states
         this.MovePlayer();
     }
 
     void Update()
     {
-        // Get walk input and update player states with animations
-        var walkInput = this.walkAction.ReadValue<Vector2>();
-        this.movement.x = this.walkSpeed * walkInput.x;
-        this.isWalking = Mathf.Abs(walkInput.x) > 1e-6f;
-        this.playerAnimations.PlayWalkAnimation(this.isWalking);
-        this.playerAnimations.FlipSprite(walkInput.x < 0);
+        // Update horizontal movement and states with animations
+        this.UpdateWalk();
+        this.UpdateSprint();
 
-        // Check if player is jumping and update states with animations
-        this.isJumping = this.jumpAction.IsPressed() && this.isGrounded;
-        this.playerAnimations.PlayJumpAnimation(this.isJumping);
+        // Update jump states with animations
+        this.UpdateGroundJump();
+        this.UpdateWallJump();
 
-        // Check if player is grounded and update gravity scale and vertical velocity
-        if (this.isGrounded)
-        {
-            this.playerRigidbody.gravityScale = this.defaultGravityScale;
-            this.playerRigidbody.linearVelocity = new Vector2(this.playerRigidbody.linearVelocity.x, 0f);
-        }
-
-        // Check if player is falling and update states with animations
-        this.isFalling = this.playerRigidbody.linearVelocity.y < 0 && !this.isGrounded;
-        this.playerAnimations.PlayFallingAnimation(this.isFalling);
-
-        // Check if player is sprinting and update states with animations
-        this.isSprinting = this.sprintAction.IsPressed() && this.isWalking;
-        this.movement.x *= this.isSprinting ? this.sprintSpeedMultiplier : 1f;
-
-        this.playerAnimations.PlaySprintAnimation(this.isSprinting);
+        // Update falling state with animations
+        this.UpdateFall();
     }
 
     private void OnDrawGizmos()
