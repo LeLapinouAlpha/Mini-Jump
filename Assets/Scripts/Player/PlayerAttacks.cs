4b8813547a6f902712a45935d2a598c00504228e
LeLapinouAlpha <t-henon@hotmail.fr>
2026-03-26 10:43:10 +0100

    fix: use counter & check animation state info for punch toggling

--- Assets/Scripts/Player/PlayerAttacks.cs
+++ Assets/Scripts/Player/PlayerAttacks.cs
@@ -10,19 +10,41 @@ public class PlayerAttacks : MonoBehaviour
     [Header("Attack actions")]
     public InputAction punchAction;
 
+    [Header("States")]
+    private bool isAttacking;
+    private bool isPunching;
+    private int punchComboCounter;
+
     void Start()
     {
         this.playerAnimations = this.GetComponent<PlayerAnimations>();
 
         // FIXME: Create custom input action asset for player attacks
-        this.punchAction = InputSystem.actions.FindAction("Attack");
+        this.punchAction = InputSystem.actions.FindAction("Punch");
+    }
+
+    void UpdatePunch()
+    {
+        this.isPunching = this.playerAnimations.animator.GetCurrentAnimatorStateInfo(0).IsName("LeftPunching") || this.playerAnimations.animator.GetCurrentAnimatorStateInfo(0).IsName("RightPunching");
+
+        if (this.punchAction.WasPressedThisFrame() && !this.isPunching)
+        {
+            if (this.punchComboCounter % 2 == 0)
+            {
+                this.playerAnimations.animator.Play("LeftPunching");
+            }
+            else
+            {
+                this.playerAnimations.animator.Play("RightPunching");
+            }
+            this.punchComboCounter++;
+        }
     }
 
     void Update()
     {
-        if (this.punchAction.WasPressedThisFrame())
-        {
-            this.playerAnimations.animator.SetTrigger("Punch");
-        }
+        this.UpdatePunch();
+
+        this.isAttacking = this.isPunching;
     }
 }
