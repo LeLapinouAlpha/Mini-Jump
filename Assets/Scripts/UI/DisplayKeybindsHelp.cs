7c43725e52f68b7ac1f72704e11cb9ac3c2cd99a
LeLapinouAlpha <t-henon@hotmail.fr>
2026-03-26 14:39:32 +0100

    fix: save input actions as an asset file

--- Assets/Scripts/UI/DisplayKeybindsHelp.cs
+++ Assets/Scripts/UI/DisplayKeybindsHelp.cs
@@ -12,24 +12,18 @@ public class DisplayKeybindsHelp : MonoBehaviour
 
     private PlayerInput playerInput;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         this.textMeshPro = this.GetComponent<TextMeshProUGUI>();
 
-        // Get the PlayerInput component
         this.playerInput = this.GetComponent<PlayerInput>();
 
-        // Get actions from the PlayerInput's action map
         this.punchAction = this.playerInput.actions.FindAction("Punch");
         this.kickAction = this.playerInput.actions.FindAction("Kick");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // GetBindingDisplayString() typically doesn't take a device parameter
-        // It shows the binding based on the currently active control scheme
         this.textMeshPro.text =
             $"Punch: {this.punchAction.GetBindingDisplayString()}\n" +
             $"Kick: {this.kickAction.GetBindingDisplayString()}";
