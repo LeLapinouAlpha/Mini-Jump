bb54b7ff7b884a9820b7635257c256e87654df89
LeLapinouAlpha <t-henon@hotmail.fr>
2026-04-13 09:18:01 +0200

    fix: merge

--- Assets/Scripts/Camera/CameraAnchorFollow.cs
+++ Assets/Scripts/Camera/CameraAnchorFollow.cs
@@ -1,53 +1,12 @@
 using System;
 using UnityEngine;
 
-[RequireComponent(typeof(Collider2D))]
 public class CameraAnchorFollow : MonoBehaviour
 {
     [Header("Components references")]
     public CameraFollow cameraFollow;
     public Transform initialTarget;
     public Transform target;
-
-    void Start()
-    {
-        this.initialTarget = this.cameraFollow.objectToFollow;
-        this.target = this.transform.GetChild(0);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            if (this.cameraFollow != null)
-            {
-                this.cameraFollow.objectToFollow = this.target;
-            }
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            if (this.cameraFollow != null)
-            {
-                this.cameraFollow.objectToFollow = this.initialTarget;
-            }
-        }
-        
-    }
-}
-using System;
-using UnityEngine;
-
-public class CameraAnchorFollow : MonoBehaviour
-{
-    [Header("Components references")]
-    public CameraFollow cameraFollow;
-    public Transform initialTarget;
-    public Transform target;
-    public Collider2D collider;
     private float initialCameraSize;
     public float cameraSize;
 
@@ -55,7 +14,6 @@ public class CameraAnchorFollow : MonoBehaviour
     {
         this.initialTarget = this.cameraFollow.objectToFollow;
         this.target = this.transform.GetChild(0);
-        this.collider = this.GetComponent<Collider2D>();
         this.initialCameraSize = this.cameraFollow.GetComponentInParent<Camera>().orthographicSize;
     }
 
